a6a960e938bef11244581dea0b65e20427f29467
John <neat_code@outlook.com>
2020-03-30 07:52:46 +0200

    separation for more metrics

---
+++ load_data.py
@@ -0,0 +1,55 @@
+import json
+import azure.cosmos.cosmos_client as cosmos_client
+import azure.cosmos.errors as errors
+import azure.cosmos.documents as documents
+import azure.cosmos.http_constants as http_constants
+import os
+import pandas as pd
+from textblob import TextBlob
+
+class DataLoader(object):
+
+    def __init__(self):
+        with open('config.json') as json_data_file:
+            config = json.load(json_data_file)
+            self.client = cosmos_client.CosmosClient(url_connection = \
+            config["cosmos_url"], auth = {"masterKey": config["cosmos_key"] })
+
+    def load_data(self):
+        tweet_collection = 'dbs/' + 'TwitterAnalysis' + '/colls/' + 'tweets'
+        query = 'SELECT top 100 * FROM c'
+        tweet_list = []
+
+        for item in self.client.QueryItems(tweet_collection,
+                                      query,
+                                      {'enableCrossPartitionQuery': True}
+                                      ):
+            tweet_list.append(dict(item))
+
+        return tweet_list
+
+
+    def format_data(self):
+        scoped_list = []
+        tweet_list = self.load_data()
+        for item in tweet_list:
+            if item['Tweet']['IsRetweet'] is False and item['Tweet']['QuotedStatusId'] is None and item['Tweet']['InReplyToStatusId'] is None:
+
+                if item['Tweet']['TweetDTO']['extended_tweet'] is not None:
+                    text = item['Tweet']['TweetDTO']['extended_tweet']['full_text']
+                else:
+                    text = item['Tweet']['TweetDTO']['text']
+
+                tb = TextBlob(text)
+                result = {
+                    "text": text,
+                    "polarity": tb.sentiment.polarity,
+                    "subjectivity": tb.sentiment.subjectivity,
+                    "aboutVirus": ("virus" in text.lower()),
+                    "aboutCorona": ("corona" in text.lower() or "covid19" in text.lower()),
+                    "aboutResistance": ("resistance" in text.lower())
+                }
+                scoped_list.append(result)
+
+
+        return pd.DataFrame(scoped_list)
--- sentiment_histograms.py
+++ sentiment_histograms.py
@@ -1,63 +1,20 @@
-import json
-import azure.cosmos.cosmos_client as cosmos_client
-import azure.cosmos.errors as errors
-import azure.cosmos.documents as documents
-import azure.cosmos.http_constants as http_constants
-import os
-import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
-from textblob import TextBlob
 from matplotlib.pyplot import subplot, figure
+from load_data import DataLoader
 
 def update_histogram(dataframe, metric_column, figure_number, title):
     chart = plt.subplot(2, 4, figure_number)
     chart.hist(dataframe[metric_column])
     chart.set_title(title)
 
-with open('config.json') as json_data_file:
-    config = json.load(json_data_file)
-
-client = cosmos_client.CosmosClient(url_connection = config["cosmos_url"], auth = {"masterKey": config["cosmos_key"] })
-tweet_collection = 'dbs/' + 'TwitterAnalysis' + '/colls/' + 'tweets'
-query = 'SELECT * FROM c'
-tweet_list = []
-
-for item in client.QueryItems(tweet_collection,
-                              query,
-                              {'enableCrossPartitionQuery': True}
-                              ):
-    tweet_list.append(dict(item))
-
-scoped_list = []
-
-for item in tweet_list:
-    if item['Tweet']['IsRetweet'] is False and item['Tweet']['QuotedStatusId'] is None and item['Tweet']['InReplyToStatusId'] is None:
-
-        if item['Tweet']['TweetDTO']['extended_tweet'] is not None:
-            text = item['Tweet']['TweetDTO']['extended_tweet']['full_text']
-        else:
-            text = item['Tweet']['TweetDTO']['text']
-
-        tb = TextBlob(text)
-        result = {
-            "text": text,
-            "polarity": tb.sentiment.polarity,
-            "subjectivity": tb.sentiment.subjectivity,
-            "aboutVirus": ("virus" in text.lower()),
-            "aboutCorona": ("corona" in text.lower() or "covid19" in text.lower()),
-            "aboutResistance": ("resistance" in text.lower())
-        }
-        scoped_list.append(result)
-
-
-df = pd.DataFrame(scoped_list)
+data_loader = DataLoader()
+df = data_loader.format_data()
 
 # Total Rows
 update_histogram(df, "polarity", 1, "Total Polarity")
 update_histogram(df, "subjectivity", 2, "Total Subjectivity")
 
-# Virus Rows
+# Corona Rows
 update_histogram(df.loc[df["aboutCorona"]], "polarity", 3, "Corona Polarity")
 update_histogram(df.loc[df["aboutCorona"]], "subjectivity", 4, "Corona Subjectivity")
 
